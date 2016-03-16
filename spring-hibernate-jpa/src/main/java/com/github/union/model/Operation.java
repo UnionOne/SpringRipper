44d79ab8918b758c2d454b8c3ccef03fe069d571
Union One <hax2033@gmail.com>
2016-03-16 21:03:53 +0300

    spring-hibernate-jpa: Create PersonRepository

--- spring-hibernate-jpa/src/main/java/com/github/union/model/Operation.java
+++ spring-hibernate-jpa/src/main/java/com/github/union/model/Operation.java
@@ -1,5 +1,53 @@
 package com.github.union.model;
 
-public class Operation {
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
+import java.math.BigDecimal;
+
+import static javax.persistence.CascadeType.ALL;
+
+public class Operation {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne(cascade = {ALL})
+    private CreditCard creditCard;
+
+    private BigDecimal amount;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public CreditCard getCreditCard() {
+        return creditCard;
+    }
+
+    public void setCreditCard(CreditCard creditCard) {
+        this.creditCard = creditCard;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        return "Operation{" +
+                "id=" + id +
+                ", creditCard=" + creditCard +
+                ", amount=" + amount +
+                '}';
+    }
 }
