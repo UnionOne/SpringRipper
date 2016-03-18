99df9290a61528c10e6b0b61c85fe3811cdb5b95
Union One <hax2033@gmail.com>
2016-03-18 12:56:03 +0300

    spring-data-jpa: Delete

--- spring-data-jpa/src/main/java/com/github/union/exception/ShopNotFoundException.java
+++
@@ -1,4 +0,0 @@
-package com.github.union.exception;
-
-public class ShopNotFoundException extends Exception {
-}
--- spring-data-jpa/src/main/java/com/github/union/repository/ShopRepository.java
+++
@@ -1,7 +0,0 @@
-package com.github.union.repository;
-
-import com.github.union.model.Shop;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ShopRepository extends JpaRepository<Shop, Integer> {
-}
