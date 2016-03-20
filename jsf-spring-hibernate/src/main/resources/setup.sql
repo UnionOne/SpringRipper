5534e48232cb99fda59c8f45c9be27ff5e2f8f76
Union One <hax2033@gmail.com>
2016-03-20 13:40:11 +0300

    jsf-spring-hibernate: Transition to PostgreSQL and little working program

--- jsf-spring-hibernate/src/main/resources/setup.sql
+++ jsf-spring-hibernate/src/main/resources/setup.sql
@@ -1,6 +1,5 @@
-CREATE TABLE `Person` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(20) NOT NULL DEFAULT '',
-  `country` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+CREATE TABLE Person(
+  id        SERIAL PRIMARY KEY NOT NULL,
+  name      VARCHAR,
+  country   VARCHAR
+);
