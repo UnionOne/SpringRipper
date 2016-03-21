9c92c369fcdd4595adb55388ae1076ce9bb8c9ec
Union One <hax2033@gmail.com>
2016-03-21 09:54:28 +0300

    spring-data-demo: Add entities

--- spring-data-demo/src/main/java/com/github/union/Talk.java
+++ spring-data-demo/src/main/java/com/github/union/Talk.java
@@ -1,4 +1,35 @@
 package com.github.union;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
 public class Talk {
+    @Id
+    @GeneratedValue
+    private Long talkId;
+
+    private Date when;
+
+    private String title;
+
+    public Talk() {
+
+    }
+
+    public Talk(Date when, String title) {
+        this.when = when;
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return this.title;
+    }
+
+    public Date getWhen() {
+        return when;
+    }
 }
